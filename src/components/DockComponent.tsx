fdbcbd6dbe419b3274a46dc7b0960fc8d1523b4a
pdaveoli <pdaveoli@monkswalk.herts.sch.uk>
2025-11-23 21:59:07 +0000

    Initial commit

---
+++ src/components/DockComponent.tsx
@@ -0,0 +1,99 @@
+﻿"use client";
+import React from "react"
+import Link from "next/link"
+import { FaHouse, FaComputer, FaGithub, FaItchIo, FaFileLines } from "react-icons/fa6";
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+import { Separator } from "@/components/ui/separator"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { Dock, DockIcon } from "@/components/ui/dock"
+
+export type IconProps = React.HTMLAttributes<SVGElement>
+const Icons = {
+    home: (props: IconProps) => <FaHouse {...props} />,
+    projects: (props: IconProps) => <FaComputer  {...props} />,
+    CV: (props: IconProps) => ( <FaFileLines {...props} /> ),
+    itchIo: (props: IconProps) => ( <FaItchIo {...props} /> ),
+    github: (props: IconProps) => ( <FaGithub {...props} /> ),
+}
+const DATA = {
+    navbar: [
+        { href: "/", icon: Icons.home, label: "Home" },
+        { href: "/projects", icon: Icons.projects, label: "Projects" },
+        { href: "#", icon: Icons.CV, label: "CV" },
+    ],
+    contact: {
+        social: {
+            GitHub: {
+                name: "GitHub",
+                url: "https://github.com/pdaveoli/",
+                icon: Icons.github,
+            },
+            itchIo: {
+                name: "Itch.io",
+                url: "https://oliverdf1.itch.io/",
+                icon: Icons.itchIo,
+            }
+        },
+    },
+}
+export function DockComponent() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <TooltipProvider>
+                <Dock direction="middle">
+                    {DATA.navbar.map((item) => (
+                        <DockIcon key={item.label}>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Link
+                                        href={item.href}
+                                        aria-label={item.label}
+                                        className={cn(
+                                            buttonVariants({ variant: "ghost", size: "icon" }),
+                                            "size-12 rounded-full"
+                                        )}
+                                    >
+                                        <item.icon className="size-4" />
+                                    </Link>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>{item.label}</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </DockIcon>
+                    ))}
+                    <Separator orientation="vertical" className="h-full" />
+                    {Object.entries(DATA.contact.social).map(([name, social]) => (
+                        <DockIcon key={name}>
+                            <Tooltip>
+                                <TooltipTrigger asChild>
+                                    <Link
+                                        href={social.url}
+                                        aria-label={social.name}
+                                        target="_blank"
+                                        className={cn(
+                                            buttonVariants({ variant: "ghost", size: "icon" }),
+                                            "size-12 rounded-full"
+                                        )}
+                                    >
+                                        <social.icon className="size-4" />
+                                    </Link>
+                                </TooltipTrigger>
+                                <TooltipContent>
+                                    <p>{name}</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </DockIcon>
+                    ))}
+                </Dock>
+            </TooltipProvider>
+        </div>
+    )
+}
+
---
+++ src/components/Hero.tsx
@@ -0,0 +1,32 @@
+﻿import {FlickeringGrid} from "@/components/ui/flickering-grid";
+import {TextAnimate} from "@/components/ui/text-animate";
+
+export default function Hero() {
+
+    // screen height:
+
+
+    return (
+        <div className="bg-background relative min-h-screen w-full overflow-hidden border items-center justify-center flex">
+            <FlickeringGrid
+                className="absolute w-full h-full"
+                squareSize={4}
+                gridGap={6}
+                color="#6B7280"
+                maxOpacity={0.5}
+                flickerChance={0.1}
+            />
+            <div className="relative z-10 flex h-full w-full flex-col items-center justify-center px-4 text-center">
+                <TextAnimate animation="blurInUp" as="h1" className="mb-4 text-4xl font-bold sm:text-5xl md:text-6xl">
+                Oliver Dave
+                </TextAnimate>
+                <p className="mb-8 max-w-2xl text-lg sm:text-xl md:text-2xl text-muted-foreground">
+                    Web and Game Development
+                </p>
+                <button className="rounded-full bg-primary px-6 py-3 text-white hover:bg-primary/80 transition">
+                    Learn More
+                </button>
+            </div>
+        </div>
+    );
+}
